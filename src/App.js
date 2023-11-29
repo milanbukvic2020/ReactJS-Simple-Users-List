8145a6354ba844df32589754575b269cdd46a09d
milanbukvic2020 <45269327+milanbukvic2020@users.noreply.github.com>
2023-11-29 03:36:31 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+//MB004
+import React, { useState } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+
+function App() {
+  const [usersList, setUserList] = useState([]);
+  //const [usersList, setUserList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUserList((prevList) => {
+      return [
+        ...prevList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+  // const addUserHandler = (uName, uAge) => {
+  //   setUserList((prevUserList) => {
+  //     return [
+  //       ...prevUserList,
+  //       { name: uName, age: uAge, id: Math.random().toString() },
+  //     ];
+  //   });
+  // };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+  // return (
+  //   <div>
+  //     <AddUser onAddUser={addUserHandler} />
+  //     <UsersList users={usersList} />
+  //   </div>
+  // );
+}
+
+export default App;
